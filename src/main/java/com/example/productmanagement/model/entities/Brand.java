cb141578ca64375e9524e3090f05e80aad72b490
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-11-20 23:41:17 -0300

    fix: removido método serId e corrigida a tipagem do atributo "id" de long para Long

--- src/main/java/com/example/productmanagement/model/entities/Brand.java
+++ src/main/java/com/example/productmanagement/model/entities/Brand.java
@@ -12,23 +12,19 @@ public class Brand {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private long id;
+  private Long id;
   private String name;
 
   public Brand() {
   }
 
-  public Brand(long id, String name) {
+  public Brand(Long id, String name) {
     this.id = id;
     this.name = name;
   }
 
-  public long getId() {
+  public Long getId() {
     return id;
-  }
-
-  public void setId(long id) {
-    this.id = id;
   }
 
   public String getName() {
