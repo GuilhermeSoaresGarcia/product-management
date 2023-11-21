80e29f3b24e29d351668b25faea55c237709941e
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-11-20 23:34:00 -0300

    fix: adicionada a annotation @Repository a classe BrandRepository

--- src/main/java/com/example/productmanagement/model/repositories/BrandRepository.java
+++ src/main/java/com/example/productmanagement/model/repositories/BrandRepository.java
@@ -2,7 +2,9 @@ package com.example.productmanagement.model.repositories;
 
 import com.example.productmanagement.model.entities.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BrandRepository extends JpaRepository<Brand, Long> {
 
 }
