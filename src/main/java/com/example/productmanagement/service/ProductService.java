b233263a0c1e9817a736ab86445591f2a53762e8
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-11-23 06:15:45 -0300

    feat: implementação das operações CRUD na camada service

--- src/main/java/com/example/productmanagement/service/ProductService.java
+++ src/main/java/com/example/productmanagement/service/ProductService.java
@@ -1,9 +1,11 @@
 package com.example.productmanagement.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import com.example.productmanagement.model.entities.Product;
 import com.example.productmanagement.model.repositories.ProductRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
@@ -15,7 +17,41 @@ public class ProductService {
     this.productRepository = productRepository;
   }
 
-  public Product insertBrand(Product product) {
+  public Product insertProduct(Product product) {
     return productRepository.save(product);
+  }
+
+  public List<Product> getAllProducts() {
+    return productRepository.findAll();
+  }
+
+  public Optional<Product> getProductById(Long id) {
+    Optional<Product> productFromDB = productRepository.findById(id);
+
+    if (productFromDB.isPresent()) {
+      return Optional.of(productFromDB.get());
+    }
+    return productFromDB;
+  }
+
+  public Optional<Product> updateProduct(Long id, Product product) {
+    Optional<Product> productFromDB = productRepository.findById(id);
+
+    if (productFromDB.isPresent()) {
+      productFromDB.get().setName(product.getName());
+      productFromDB.get().setPrice(product.getPrice());
+      return Optional.of(productRepository.save(productFromDB.get()));
+    }
+    return productFromDB;
+  }
+
+  public Optional<Product> deleteProduct(Long id) {
+    Optional<Product> productFromDB = productRepository.findById(id);
+    
+    if (productFromDB.isPresent()) {
+      productRepository.deleteById(id);
+    }
+    
+    return productFromDB;
   }
 }
