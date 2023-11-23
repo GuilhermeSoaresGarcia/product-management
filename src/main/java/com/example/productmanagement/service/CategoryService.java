149bdca7c92bf14b783deef27733d38b2cede447
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-11-22 23:32:37 -0300

    feat: criada classe CatgeoryService da camada service e métodos CRUD

---
+++ src/main/java/com/example/productmanagement/service/CategoryService.java
@@ -0,0 +1,58 @@
+package com.example.productmanagement.service;
+
+import com.example.productmanagement.model.entities.Category;
+import com.example.productmanagement.model.repositories.CategoryRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+  private CategoryRepository categoryRepository;
+
+  @Autowired
+  public CategoryService(CategoryRepository categoryRepository) {
+    this.categoryRepository = categoryRepository;
+  }
+
+  public Category insertCategory(Category category) {
+    return categoryRepository.save(category);
+  }
+
+  public Optional<Category> getCategoryById(Long id) {
+    Optional<Category> optionalCategory = categoryRepository.findById(id);
+
+    if (optionalCategory.isPresent()) {
+      return Optional.of(optionalCategory.get());
+    }
+    return optionalCategory;
+  }
+
+  public List<Category> getAllCategorys() {
+    return categoryRepository.findAll();
+  }
+
+  public Optional<Category> updateCategory(Long id, Category category) {
+    Optional<Category> optionalCategory = categoryRepository.findById(id);
+
+    if (optionalCategory.isPresent()) {
+      Category categoryFromDB = optionalCategory.get();
+      categoryFromDB.setName(category.getName());
+
+      Category updatedCategory = categoryRepository.save(categoryFromDB);
+      return Optional.of(updatedCategory);
+    }
+    return optionalCategory;
+  }
+
+  public Optional<Category> deleteCategory(Long id) {
+    Optional<Category> optionalCategory = categoryRepository.findById(id);
+
+    if (optionalCategory.isPresent()) {
+      categoryRepository.deleteById(id);
+    }
+    return optionalCategory;
+  }
+}
